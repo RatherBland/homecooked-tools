4b1d9caff972f914aefd3f9869072a07b341c9d3
RatherBland <guy@ratherbland.com>
2018-04-26 11:45:01 +1000

    Added comments to html-analysis.py

--- Web Static Analysis/html-analysis.py
+++ Web Static Analysis/html-analysis.py
@@ -1,45 +1,52 @@
 import urllib2
 from bs4 import BeautifulSoup, Comment
-import re, urllib
+import re
 
+# Initiate array to store discovered links
 urllist = []
 
+# Initate global variables, these will be inputted via cmd line arg in the future
 myurl = 'https://googleprojectzero.blogspot.com.au/2018/04/windows-exploitation-tricks-exploiting.html'
 host = 'https://googleprojectzero\.blogspot\.com\.au'
 base = 'https://googleprojectzero.blogspot.com.au'
 
+# Generate regex string so to only crawl the desired application
 expressions_string = '''href=["']''' + host + '''(.[^"']+)["']'''
 
-for reference in re.findall(expressions_string, urllib.urlopen(myurl).read(), re.I):
+# Begin crawling of application
+for reference in re.findall(expressions_string, urllib2.urlopen(myurl).read(), re.I):
         
+        # Ensure that links that already exist in the urllist array are not added again
         if reference not in urllist and '#' not in reference and 'search?' not in reference:
                 urllist.append(reference)
                 link = base + reference
                 print link                
 
-        for layered_reference in re.findall(expressions_string, urllib.urlopen(base + reference).read(), re.I):
-                        
+        for layered_reference in re.findall(expressions_string, urllib2.urlopen(base + reference).read(), re.I):
+                
+                # Ensure that link does not contain '#', these links direct to local elements of a page        
                 if layered_reference not in urllist and '#' not in layered_reference:
                         urllist.append(layered_reference)
                         layered_link = base + layered_reference
                         print layered_link                                
         
+# Initate comments array
 recon_comments = []
 
+# Begin scraping of comments from crawled links
 for page in urllist:
+        
+        # Generate links to crawl from urllist array
         link = urllib2.urlopen(base + page)
         html = link.read()
 
         soup = BeautifulSoup(html,'html.parser')
-
+        
+        # Return the HTML for page and determine comments
         for comments in soup.findAll(text=lambda text:isinstance(text, Comment)):
                 
-                # comment_length = len(comments.extract())
+                # Ensure comments that already exist in the recon_comments array are not added again
                 if comments.extract() not in recon_comments:
                         
                         recon_comments.append(comments.extract())
                         print comments.extract()
-                
-                # print '-' * comment_length
-                # print '\n'
-    
