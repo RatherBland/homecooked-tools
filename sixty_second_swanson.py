36deb8017b6d1e4fe311f7c22fd276bd69c32197
Guy <25998850+RatherBland@users.noreply.github.com>
2018-05-07 14:32:23 +1000

    Neatened code a tad

--- sixty_second_swanson.py
+++ sixty_second_swanson.py
@@ -7,24 +7,33 @@ URL = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes'
 API_KEY = pushbullet_key.get_key()
 
 try:
+    
     PB = Pushbullet(API_KEY)
 
 except Exception as ex:
-    print ex
+    
+    print(ex)
     
 def swanson():
     
-    RESPONSE = requests.get(URL)
-    if RESPONSE.text not in USED_QUOTES:
+    try:
         
-        USED_QUOTES.append(RESPONSE.text)
-        QUOTE = RESPONSE.text.replace('[', '', 1)
-        NEW_QUOTE = QUOTE.replace(']', '', 1)
-        PUSH = PB.push_note('Sixty Second Swanson', NEW_QUOTE)
+        RESPONSE = requests.get(URL)
+        if RESPONSE.text not in USED_QUOTES:
+            
+            USED_QUOTES.append(RESPONSE.text)
+            QUOTE = RESPONSE.text.replace('[', '', 1)
+            NEW_QUOTE = QUOTE.replace(']', '', 1)
+            PUSH = PB.push_note('Sixty Second Swanson', NEW_QUOTE)
     
-    else:
-        swanson()
+        else:
+            swanson()
+            
+    except Exception as ex:
+        
+        print('Uh oh, looks like something went wrong: %s' %ex)
     
 while True:
+    
     swanson()
     time.sleep(60)
